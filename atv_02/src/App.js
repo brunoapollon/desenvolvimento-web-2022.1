bf24b6f7edfaab0e5c55b41dd1b367dfd86c00bd
Bruno Lopes <eusoubruno7@gmail.com>
2022-04-17 14:22:40 -0300

    feat: adding structure of activity 2

---
+++ atv_02/src/App.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "./App.css";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+import CreateStudent from "./components/crud/students/CreateStudent";
+import ListStudent from "./components/crud/students/ListStudent";
+import EditStudent from "./components/crud/students/EditStudent";
+
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to={"/"} className="navbar-brand" style={{ paddingLeft: 10 }}>
+          CRUD
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="navitem">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="navitem">
+              <Link to="/createStudent" className="nav-link">
+                Create Student
+              </Link>
+            </li>
+            <li className="navitem">
+              <Link to="/listStudent" className="nav-link">
+                List Student
+              </Link>
+            </li>
+            <li className="navitem">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="createStudent" element={<CreateStudent />} />
+        <Route path="listStudent" element={<ListStudent />} />
+        <Route path="editStudent/:id" element={<EditStudent />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ atv_02/src/components/crud/students/ListStudent.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import StudentTableRow from "./StudentTableRow";
+import { students } from "./data.js";
+
+function ListStudent() {
+  function generateTable() {
+    if (!students) return;
+    return students.map((student, i) => {
+      return <StudentTableRow student={student} key={i} />;
+    });
+  }
+
+  return (
+    <>
+      <main>
+        <h2>List Student</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Curso</th>
+              <th>IRA</th>
+              <th colSpan={2} style={{ textAlign: "center" }}>
+                Ações
+              </th>
+            </tr>
+          </thead>
+          <tbody>{generateTable()}</tbody>
+        </table>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+export default ListStudent;
