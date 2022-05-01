a3d5b5fcad09f25b8b424e922f939f387034ecaf
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-01 14:42:56 -0300

    feat: adding activity three structure

---
+++ atv_03/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+var studentRoute = require("./routes/student/StudentRoute");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+//app.use('/api/v1/users', users);
+app.use("/crud/students", studentRoute);
+
+module.exports = app;
